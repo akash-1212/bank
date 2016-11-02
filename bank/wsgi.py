b363fa4d71eb0c31ed6bbc656810837b6f614679
akash-1212 <aakashbarapatre@gmail.com>
2016-11-02 19:12:56 +0530

    :added .ebextensions

--- bank/wsgi.py
+++ bank/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.insert(0, '/opt/python/current/app')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bank.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bank.settings")
 
 application = get_wsgi_application()
